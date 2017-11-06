5f4fd7860749362c5cbbbb4db7ff1361c0962d1d
Jack Heseltine <jack.heseltine@gmail.com>
2017-11-06 16:13:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function Point(x, y) {
 Point.prototype.toString = function() {
   return '(' + this.x + ', ' + this.y + ')'
 }
+
+function Shape() {
+  //this.position = '';
+}
+
+Shape.prototype.addToPlane = function(x, y) {
+  this.position = new Point(x, y);
+}
+
+Shape.prototype.move = function(x, y) {
+  this.position.x = x;
+  this.position.y = y;
+}
+
